c1761473651987c04acb952a22a52fc15b49d5c0
Loh Chun Mun <chunmun_loh@live.com>
2022-10-11 20:36:14 +0800

    update run batch script

--- run.bat
+++ run.bat
@@ -2,6 +2,11 @@
 @REM JAVA_HOME - location of a JDK 17 home dir (change path if necessary)
 set JAVA_HOME=C:\Program Files\Java\jdk-17.0.2
 
+cd .\frontend
+start cmd /c npm start
+cd ..
+
+
 cd .\backend
 start cmd /c mvnw spring-boot:run
 
