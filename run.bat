39b087fcf5ab590bebe4f82f55fca6c09265dbf2
Loh Chun Mun <chunmun_loh@live.com>
2022-10-11 19:53:52 +0800

    add run batch script

---
+++ run.bat
@@ -0,0 +1,8 @@
+@REM Required ENV vars:
+@REM JAVA_HOME - location of a JDK 17 home dir (change path if necessary)
+set JAVA_HOME=C:\Program Files\Java\jdk-17.0.2
+
+cd .\backend
+start cmd /c mvnw spring-boot:run
+
+exit
