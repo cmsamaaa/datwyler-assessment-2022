1de04096c41fb58c14da392854599c92f5bb856a
Loh Chun Mun <chunmun_loh@live.com>
2022-10-11 19:51:37 +0800

    added CORS origin to backend

--- backend/src/main/java/com/datwyler/assessment/backend/Controller/BankController.java
+++ backend/src/main/java/com/datwyler/assessment/backend/Controller/BankController.java
@@ -2,6 +2,7 @@ package com.datwyler.assessment.backend.Controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import com.datwyler.assessment.backend.Model.Applicant;
 import com.datwyler.assessment.backend.Model.CreditFacility;
 import com.datwyler.assessment.backend.Model.Loan;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class BankController {
 	
